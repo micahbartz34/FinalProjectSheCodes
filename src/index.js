9b6ef6be56d54116c14c2e1e830680763d8f32a5
micahbartz34 <77466525+micahbartz34@users.noreply.github.com>
2021-01-23 18:31:53 -0600

    Updates

--- src/index.js
+++ src/index.js
@@ -22,6 +22,7 @@ function formatDate(date) {
 }
 
 function displayWeatherCondition(response) {
+  let iconElement = document.querySelector("#icon");
   document.querySelector("#city").innerHTML = response.data.name;
   document.querySelector("#temperature").innerHTML = Math.round(
     response.data.main.temp
@@ -32,6 +33,11 @@ function displayWeatherCondition(response) {
   );
   document.querySelector("#description").innerHTML =
     response.data.weather[0].main;
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 function searchCity(city) {
