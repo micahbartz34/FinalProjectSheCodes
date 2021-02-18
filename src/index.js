24232bc244920d36694d46507322b2136129b5ea
micahbartz34 <77466525+micahbartz34@users.noreply.github.com>
2021-02-18 12:10:59 -0600

    Remove Current Location button

--- src/index.js
+++ src/index.js
@@ -82,19 +82,11 @@ function searchLocation(position) {
   axios.get(apiGeoLocation).then(parseResponse);
 }
 
-function getCurrentLocation(event) {
-  event.preventDefault();
-  navigator.geolocation.getCurrentPosition(searchLocation);
-}
-
 let dateElement = document.querySelector("#date");
 let currentTime = new Date();
 dateElement.innerHTML = formatDate(currentTime);
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", handleSubmit);
-
-let currentLocationButton = document.querySelector("#current-location-button");
-currentLocationButton.addEventListener("click", getCurrentLocation);
 
 searchCity("Chicago");
